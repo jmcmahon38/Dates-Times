7da451fa7c436ae835e49c2d718097a9b12c234c
jmcmahon38 <jmcmahon38@students.mchenry.edu>
2021-10-04 17:10:45 -0500

    added some formatting changes, fixed incorrect formatting for one of the moon dates

--- src/main.kt
+++ src/main.kt
@@ -1,31 +1,37 @@
 import java.time.LocalDate
-import java.time.LocalDate.now
+
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 import java.util.*
 
-fun main(args: Array<String>) {
-// Next full moon 10/20/2021
-// Current date
-    val date = LocalDate.now()
+// Design a program that will determine when the next full moon will be based upon the current date.
 
+fun main(args: Array<String>) {
+    // Next full moon 10/20/2021
+    // Current date
+    val date = LocalDate.now()
+    // format for dates
     val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
+    //formatting today's day to preferred pattern
     val today = date.format(formatter)
 
+    // container for next full and following full moon value
     val nextFullMoon : LocalDate = LocalDate.of(2021, 10, 20)
-    // how do I convert this to a new format?
     val followingNextFullMoon : LocalDate = LocalDate.of(2021, 11, 19)
+    // calculate days between full moons
     val daysToNextMoon = date.until(nextFullMoon, ChronoUnit.DAYS)
     val daysToFollowingNextFullMoon = nextFullMoon.until(followingNextFullMoon, ChronoUnit.DAYS)
 
-    println("today is: ${today.format(formatter)}\n" +
-            "next full moon is: $nextFullMoon\n" +
+    // print date, date for next moon, how many days until next full moon
+    // and days between the moon after that
+    println("today is: ${today}\n" +
+            "next full moon is: ${nextFullMoon.format(formatter)}\n" +
             "Time between now and next full moon: $daysToNextMoon\n" +
             "Time between the next full moons: $daysToFollowingNextFullMoon\n")
 
-
+}
 
 
 // notes
@@ -33,7 +39,7 @@ fun main(args: Array<String>) {
 //    val dateTime = LocalDateTime.now()
 //    println(dateTime)
 
-// Curent date
+// Current date
 //    val date = LocalDate.now()
 //    println(date)
 
@@ -72,9 +78,7 @@ fun main(args: Array<String>) {
 //        println("Your next birth is: $nextBirthday and days are $daysToBirthday")
 //    }
 
-    // Design a program that will determine when the next full moon will be based upon the current date.
 
 
 
 
-}
